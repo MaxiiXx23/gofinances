8d7315f0e18cdbe39cd5811c8a72ba55e4b9c017
MaxiiXx23 <max.232017@gmail.com>
2022-09-26 15:58:10 -0300

    Screen CategorySelect feita, adição de modal ao Regiter e refatoração.

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -6,7 +6,7 @@ export default {
         success: "#12A454",
         success_light: "rgba(18, 164, 84, 0.5)",
         attention: "#E83F5B",
-        attention_light: "gba(232, 63, 91, 0.5)",
+        attention_light: "rgba(232, 63, 91, 0.5)",
         shape: "#FFFFFF",
         title: "#363F5F",
         text_dark: "#000000",
