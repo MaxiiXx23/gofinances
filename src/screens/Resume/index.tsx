da1f13d7a948b77cfbb942d5e93fea65fead3bcd
MaxiiXx23 <max.232017@gmail.com>
2022-10-05 22:07:48 -0300

    Componente HistoryCard feito.

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { HistoryCard } from "../../components/HistoryCard";
+
+import { 
+    Container, 
+    Header, 
+    Title 
+} from "./styles";
+
+
+export function Resume (){
+    return(
+        <Container>
+            <Header>
+                <Title>Resume</Title>
+            </Header>
+            <HistoryCard 
+                title="Compras"
+                amount="R$150,50"
+                color="red"
+            />
+        </Container>
+    )
+}
